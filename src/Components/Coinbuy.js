0aa9312c99d75b49651e7ab8a789845ac2370c95
Minhaj Ahmad Khan <51471123+minhajspace@users.noreply.github.com>
2022-02-10 14:04:50 +0530

    added button

---
+++ src/Components/Coinbuy.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+
+function CoinBuy ({filteredWallet, parent}) {
+    console.log("filteredWallet, parent==",filteredWallet, parent)
+  return <>
+    
+
+         </>
+}
+
+export default CoinBuy
--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -8,6 +8,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CoinBuy from './Coinbuy'
 
 
 const bull = (
@@ -22,22 +23,7 @@ const bull = (
 
 const Wallet = ({ filteredWallet, parent, setParent, setShowLink }) => {
 
-    const increaseHandle = (wallet) => {
-        setParent(parent.map((el) => (el.id === wallet.id && wallet.coinvolume > 0) ? { ...el, coinvolume: wallet.coinvolume + 1 } : el))
-    }
-
-    const decreaseHandle = (wallet) => {
-        setParent(parent.map((el) => (el.id === wallet.id && wallet.coinvolume > 1) ? { ...el, coinvolume: wallet.coinvolume - 1 } : el))
-    }
-
-    const deleteHandle = (wallet) => {
-        toast.success("Coin deleted successfully", {
-            position: "top-center",
-            autoClose: 2000
-        })
-        setParent(parent.filter((el) => el.id !== wallet.id))
-    }
-
+   
     return (
 
         <>
@@ -45,6 +31,10 @@ const Wallet = ({ filteredWallet, parent, setParent, setShowLink }) => {
              <div className='d-flex'>  
                     <Button className="flex-1 btn-common buy-btn">Buy</Button>
                     <Button className="flex-1 sell-btn btn-common">Sell</Button>
+             </div>
+             <div className='d-flex'>
+                    <CoinBuy filteredWallet={filteredWallet} parent={parent}/>
+
              </div>
       
          </Card>
